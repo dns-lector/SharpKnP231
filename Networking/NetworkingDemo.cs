765c3ba81536836a83755b40eed66a117a025b90
dns-lector <157026463+dns-lector@users.noreply.github.com>
2026-02-20 11:41:32 +0200

    Xml

--- Networking/NetworkingDemo.cs
+++ Networking/NetworkingDemo.cs
@@ -2,15 +2,53 @@
 using SharpKnP321.Networking.Orm;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SharpKnP321.Networking
 {
     internal class NetworkingDemo
     {
-        public async Task Run()
+        public async Task Run()  // Xml
+        {
+            using HttpClient client = new();
+            HttpRequestMessage request = new()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange"),
+            };
+            Task<HttpResponseMessage> responseTask =
+                client.SendAsync(request);
+
+            Console.WriteLine("Курси валют НБУ, робота з XML");
+            Console.Write(DateTime.Now.Ticks / 10000 % 100000); Console.WriteLine(" request start");
+
+            HttpResponseMessage response = await responseTask;
+            Task<String> contentTask = response.Content.ReadAsStringAsync();
+
+            Console.Write(DateTime.Now.Ticks / 10000 % 100000); Console.WriteLine(" request finish");
+            String xmlString = await contentTask;
+            // Console.WriteLine(xmlString);
+            XDocument xmlDocument = XDocument.Parse(xmlString);
+            // Console.WriteLine(xmlDocument.Descendants("currency").Count());
+            List<NbuRate> rates = [..
+                xmlDocument
+                .Descendants("currency")
+                .Select(c =>  new NbuRate
+                {
+                    Txt = c.Element("txt")!.Value,
+                    Rate = Convert.ToDouble(c.Element("rate")!.Value, CultureInfo.InvariantCulture),
+                    Cc = c.Element("cc")!.Value,
+                    R030 = Convert.ToInt32(c.Element("r030")!.Value),
+                })
+            ];
+            Console.WriteLine(String.Join('\n', rates));
+        }
+
+        public async Task RunJson()
         {
             // Курси валют НБУ, робота з JSON
 
@@ -83,6 +121,7 @@ namespace SharpKnP321.Networking
  * що її вводить користувач з клавіатури.
  * Забезпечити перевірку валідності дати, а також те, що вона
  * належить минулому
+ * !! Використати формат XML
  * https://bank.gov.ua/ua/open-data/api-dev
  */
 /* Д.З. Реалізувати завантажувач кодів сайтів:
@@ -121,4 +160,27 @@ namespace SharpKnP321.Networking
  * MKCOL
  * 
  * 
+ * JSON (JavaScript Object Notation)
+ * - primitive: number, "string\u10AF", null, true, false
+ * - array: [JSON, JSON, ...]
+ * - object: {"key1": JSON, "key2": JSON, ...}
+ * 
+ * XML:
+ * <?xml version="1.0" encoding="utf-8"?>
+ * <root>
+       <currency>
+            <r030>36</r030>
+            <txt>Австралійський долар</txt>
+            <rate>30.4814</rate>
+            <cc>AUD</cc>
+            <exchangedate>20.02.2026</exchangedate>
+            <special>
+            </special>
+      </currency>
+ * </root>
+ * 
+ * <?xml version="1.0" encoding="utf-8"?>
+ * <root exchangedate="20.02.2026">
+        <currency r030="36" txt="Австралійський долар" rate="30.4814"/>
+ * </root>
  */
